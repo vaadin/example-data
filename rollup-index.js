e7219af8a106a45b1620ab2610faf4de2dd3228f
Artur Signell <artur@vaadin.com>
2021-08-11 14:52:44 +0300

    Add missing file

---
+++ rollup-index.js
@@ -0,0 +1,8 @@
+const cdigit = require('cdigit');
+const chance = require('chance');
+const { v4: uuidv4 } = require('uuid');
+
+exports.cdigit = cdigit;
+exports.chance = chance;
+exports.uuidv4 = uuidv4;
+
