cc7500caa9a8adb6b7bd8b786a0466bbcb0f2c90
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2021-08-12 10:54:48 +0300

    Remove newline

--- downloadBookImages.sh
+++ downloadBookImages.sh
@@ -4,11 +4,9 @@ width=200
 height=$(($width*2))
 
 rm -f src/main/resources/com/vaadin/exampledata/BookImageBackground.txt
-for baseurl in `cat src/main/resources/com/vaadin/exampledata
-/BookImageBackground.src`;
+for baseurl in `cat src/main/resources/com/vaadin/exampledata/BookImageBackground.src`;
 do 
   url="$baseurl?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=$width&h=$height&q=80"
   echo "Downloading $url"
-  (echo -n "data:image/png;base64," ; curl -s "$url"|base64  ) >>
-  src/main/resources/com/vaadin/exampledata/BookImageBackground.txt
+  (echo -n "data:image/png;base64," ; curl -s "$url"|base64  ) >>  src/main/resources/com/vaadin/exampledata/BookImageBackground.txt
 done
